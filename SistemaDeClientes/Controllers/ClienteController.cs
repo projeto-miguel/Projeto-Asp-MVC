2b65c5bdf5629362ba831d6254435ff1990fdf74
projeto-miguel <miguel2020carneiro@gmail.com>
2023-06-20 12:35:22 -0300

    projeto MVC

---
+++ SistemaDeClientes/Controllers/ClienteController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaDeClientes.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Excluir()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+    }
+}
